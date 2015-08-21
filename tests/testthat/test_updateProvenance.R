59f689b12fea4e3879dacb163f3ef6a0ba2ca074
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2015-08-21 14:15:22 -0400

    Test code, bug fixes, split into new files

--- tests/testthat/test_updateProvenance.R
+++ tests/testthat/test_updateProvenance.R
@@ -10,10 +10,26 @@ test_that("handles bad input", {
 })
 
 test_that("updates correctly", {
-  m <- "message1"
-  d <- updateProvenance(cars, "message1")
-  p <- provenance(d)
-  expect_equal(nrow(p), 1)
-  expect_equal(p$message, m)
-  expect_equal(p$digest, digest(d))
+
+  e <- environment()    # we don't want to mess up global state, here
+
+  x <- 10
+  expect_error(updateProvenance(x, "message", env = e))  # provenance not created yet
+
+  x <- createProvenance(x, env = e)
+
+  m1 <- "message1"
+  updateProvenance(x, m1, env = e)
+  p1 <- provenance(x, env = e)
+  expect_equal(nrow(p1), 1)
+  expect_equal(p1$message, m1)
+  expect_equal(p1$digest, digest(x))
+
+  x <- x + 1
+  m2 <- "message2"
+  updateProvenance(x, m2, env = e)
+  p2 <- provenance(x, env = e)
+  expect_equal(nrow(p2), 2)
+  expect_equal(p2$message[2], m2)
+  expect_false(p2$digest[1] == p2$digest[2])
 })
